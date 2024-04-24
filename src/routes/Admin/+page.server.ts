086b40181cdb6ca75a3ed82421caf7b58f467e2d
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-04-24 08:40:23 +0100

    Remove unused database import and commented-out code in Admin page server load function

--- src/routes/Admin/+page.server.ts
+++ src/routes/Admin/+page.server.ts
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 import type { Action, Actions, PageServerLoad } from './$types';
-import { db } from '$lib/database';
 
 export const load: PageServerLoad = async (session) => {
 	var sessionData = session.cookies.get('session');
@@ -8,9 +7,4 @@ export const load: PageServerLoad = async (session) => {
 		return redirect(303, '/Login');
 	}
 	return {};
-
-	// redirect user if not logged in
-	// if (data?.role?.name !== 'ADMIN') {
-	// 	redirect(302, '/');
-	// }
 };
