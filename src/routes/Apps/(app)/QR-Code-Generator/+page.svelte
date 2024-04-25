d42f4c25f4ea66d30788f1afd239b7e7ca8baa59
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-04-25 22:16:25 +0100

    Enhance QR Code Generator page with SEO tags, dynamic welcome message, and accessibility improvements

--- src/routes/Apps/(app)/QR-Code-Generator/+page.svelte
+++ src/routes/Apps/(app)/QR-Code-Generator/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { QRCodeImage } from 'svelte-qrcode-image';
 	import * as htmlToImage from 'html-to-image';
+	import { page } from '$app/stores';
 	let inputText = '';
 
 	async function saveQRCode() {
@@ -20,11 +21,34 @@
 	}
 </script>
 
-<div
+<svelte:head>
+	<title>QR Code Generator</title>
+	<meta
+		name="description"
+		content="Generate and download QR codes for links, contact info, or any data you want to embed."
+	/>
+	<meta name="keywords" content="QR Code, QR Code Generator, Encode QR, Save QR" />
+	<script type="application/ld+json">
+		{
+			"@context": "http://schema.org",
+			"@type": "WebApplication",
+			"name": "QR Code Generator",
+			"description": "A tool to generate and download QR codes for various types of data."
+		}
+	</script>
+</svelte:head>
+
+<section
 	class="flex max-h-screen flex-col items-center justify-center text-green-900 dark:text-green-100"
 >
-	<h1 class="mb-4 text-4xl font-bold">Welcome to QR Code Generator</h1>
-	<span class="px-10">
+	<header class="mb-4 text-4xl font-bold">
+		<h1>
+			Welcome {#if $page.data.user}
+				<span class="text-green-400">{$page.data.user.username}</span>,
+			{/if} to QR Code Generator
+		</h1>
+	</header>
+	<article class="px-10">
 		<p class="mb-4 text-lg">
 			QR codes let you share information quickly. Use them for links, contact info, or any data you
 			want to embed.
@@ -33,27 +57,29 @@
 			Enter the text you want to encode into a QR code below. Once generated, share the QR code to
 			let others scan and access the information instantly.
 		</p>
-	</span>
+	</article>
 
-	<div class="w-full max-w-md">
-		<div class="flex flex-col">
+	<main class="w-full max-w-md">
+		<form class="flex flex-col">
 			<input
 				type="text"
 				placeholder="Enter text here"
+				aria-label="Input for QR code text"
 				class="mb-4 rounded-md border p-3 focus:border-green-500 focus:outline-none dark:text-green-100"
 				bind:value={inputText}
 			/>
-		</div>
-	</div>
+		</form>
+	</main>
 	{#if inputText}
-		<div class="mt-8">
+		<aside class="mt-8">
 			<QRCodeImage text={inputText} displayID="qr-code-image" displayClass="h-64 w-64 rounded-md" />
-		</div>
+		</aside>
 		<button
 			on:click={saveQRCode}
+			aria-label="Save QR Code"
 			class="mt-4 rounded-md bg-green-500 p-2 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
 		>
 			Save QR Code
 		</button>
 	{/if}
-</div>
+</section>
