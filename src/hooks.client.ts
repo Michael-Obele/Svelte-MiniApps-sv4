e557b342c17489cb87212d0e76ead754e591665c
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-05-04 07:37:02 +0100

    Integrate Sentry for error tracking and session replay in SvelteKit app

---
+++ src/hooks.client.ts
@@ -0,0 +1,21 @@
+import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://08ab6118216e73fe2743866a40174b41@o4507196497854464.ingest.de.sentry.io/4507196654944336',
+  tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [replayIntegration()],
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,11 @@
-export { handle } from "$lib/auth"
+import { sequence } from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://08ab6118216e73fe2743866a40174b41@o4507196497854464.ingest.de.sentry.io/4507196654944336',
+	tracesSampleRate: 1
+});
+
+export { handle } from '$lib/auth';
+export const handleError = Sentry.handleErrorWithSentry();
+export const sentryHandle = sequence(Sentry.sentryHandle());
