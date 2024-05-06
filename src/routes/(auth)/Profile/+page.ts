a9ad87032aa0998c5534df5207226e903dc98499
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-05-06 21:23:54 +0100

    Refactor Profile page and cleanup unused code in Svelte app

--- src/routes/(auth)/Profile/+page.ts
+++ src/routes/(auth)/Profile/+page.ts
@@ -15,34 +15,3 @@ export async function _fetchCommitData(owner: string, repo: string, branch: stri
 	}
 	return commitResponse.json();
 }
-
-// const owner = 'Michael-Obele';
-// 	const repo = 'Svelte-MiniApps';
-// 	const baseUrl = 'https://api.github.com';
-// 	const branch = 'master'; // Replace with desired branch if needed
-
-// 	const url = `${baseUrl}/repos/${owner}/${repo}/branches/${branch}`;
-
-// 	try {
-// 		const response = await fetch(url);
-
-// 		if (!response.ok) {
-// 			throw new Error(`Failed to fetch branch information: ${response.statusText}`);
-// 		}
-
-// 		const data = await response.json();
-// 		const commitSHA = data.commit.sha;
-
-// 		// Fetch the commit details using the SHA
-// 		const commitUrl = `${baseUrl}/repos/${owner}/${repo}/commits/${commitSHA}`;
-// 		const commitResponse = await fetch(commitUrl);
-
-// 		if (!commitResponse.ok) {
-// 			throw new Error(`Failed to fetch commit details: ${commitResponse.statusText}`);
-// 		}
-
-// 		const commitData  = commitResponse.json();
-// 		// let gitMessage = commitData.commit.message;
-
-// 		return { commitData }; // Pass both to your component
-//     }
