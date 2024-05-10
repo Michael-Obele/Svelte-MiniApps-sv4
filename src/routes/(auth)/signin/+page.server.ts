e801b105bb9598625549e69d552ddfbea0c3118e
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-05-10 21:35:17 +0100

    Add session check and redirect to homepage if session exists in signin page server load

--- src/routes/(auth)/signin/+page.server.ts
+++ src/routes/(auth)/signin/+page.server.ts
@@ -1,4 +1,13 @@
 import { signIn } from '$lib/auth';
-import type { Actions } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (event) => {
+	const sessionID = event.cookies.get('session');
+	const session = await event.locals.getSession();
+	if (sessionID || session) {
+		return redirect(303, '/');
+	}
+};
 
 export const actions = { default: signIn } satisfies Actions;
