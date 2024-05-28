543382cf9b29707d1afa3c425f9540b4a164344f
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-05-28 08:44:07 +0100

    Refactor English-Dictionary-App server page by removing unused imports and fetch function

--- src/routes/Apps/(app)/English-Dictionary-App/+page.server.ts
+++ src/routes/Apps/(app)/English-Dictionary-App/+page.server.ts
@@ -1,7 +1,4 @@
 import type { Actions, PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
-import { page } from '$app/stores';
-import type { PageLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {};
@@ -48,28 +45,6 @@ interface Definition {
 	antonyms?: string[];
 	example?: string;
 }
-const _fetchDictionaryEntry = async (word: string): Promise<DictionaryEntry | ApiError> => {
-	const encodedWord = encodeURIComponent(word);
-	const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodedWord}`;
-	try {
-		const response = await fetch(apiUrl);
-
-		if (!response.ok) {
-			const errorData: ApiError = await response.json();
-			return errorData; // Return the error data directly
-		}
-		const data: DictionaryEntry = await response.json();
-		return data;
-	} catch (error) {
-		console.error('Network error:', error);
-		// Construct and return a custom error object
-		return {
-			title: 'Network Error',
-			message: 'Failed to fetch dictionary entry',
-			resolution: 'Please try again later.'
-		};
-	}
-};
 
 export const actions: Actions = {
 	default: async ({ request }) => {
