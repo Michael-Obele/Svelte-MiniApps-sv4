0cc6c3af34009c20af82aa80892be719fc48efcb
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-06-07 17:37:13 +0100

    feat: Add under construction message to Advanced Emoji Tools page

--- src/routes/Apps/(app)/Advanced-Emoji-Tools/+page.svelte
+++ src/routes/Apps/(app)/Advanced-Emoji-Tools/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
+	import Construction from '$lib/logo/Construction.svelte';
 	export let data: PageData;
 </script>
+
+<section class="p-4 text-center">
+	<h3 class="mb-4 text-2xl font-bold">Under Construction</h3>
+	<p class="mb-4 text-lg">We're currently working on this site. Please check back later.</p>
+	<Construction />
+</section>
