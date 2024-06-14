cb770a75d564de4a4399a61b8744f5819e365ccf
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-06-14 01:47:05 +0100

    feat: Add error logging with Sentry in +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,16 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions, RequestEvent } from './$types';
+import * as Sentry from '@sentry/node';
 
 export const load: PageServerLoad = async (event) => {
-	console.log(event.url.href);
+	try {
+		// Your existing code or logic that might fail
+		console.log(event.url.href);
+		throw new Error(`URL Accessed: ${event.url.href}`);
+	} catch (error) {
+		Sentry.captureException(error);
+	}
+
 	if (event.url.href == 'https://svelte-mini-apps.netlify.app/') {
 		redirect(301, 'https://svelte-apps.me/');
 	}
