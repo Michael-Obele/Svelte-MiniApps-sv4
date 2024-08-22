43719a23fedacd685ec2b9f21b2fddf6b34d2f17
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-08-22 21:39:22 +0100

    feat ✨: update login logic and refactor ♻️ viewPasswords action

--- src/routes/(auth)/Login/+page.server.ts
+++ src/routes/(auth)/Login/+page.server.ts
@@ -6,7 +6,7 @@ const db = getDbInstance(); // Get the Prisma client instance
 export const load: PageServerLoad = async (event) => {
 	const sessionID = event.cookies.get('session');
 	const session = await event.locals.getSession();
-	if (sessionID && session) {
+	if (sessionID || session) {
 		return redirect(303, '/Profile');
 	}
 	return {};
--- src/routes/(auth)/Profile/+page.server.ts
+++ src/routes/(auth)/Profile/+page.server.ts
@@ -11,41 +11,41 @@ export const load: PageServerLoad = async (event) => {
 	}
 };
 export const actions: Actions = {
-// View action to display saved passwords for a user
-viewPasswords: async ({ request }) => {
-	const data = await request.formData();
-	let id = String(data.get('id'));
-	// Ensure id is a string or undefined, not null
-	// Check if the user exists in the database
-	const user = await db.user.findUnique({ where: { id } });
-	if (!user) {
-		return fail(404, { invalid: true });
-	}
-	/**
-	 * Specifies fields to select from password records using Prisma's Select added to the model name SavePassword.
-	 * Only works for select to give `SavePasswordSelect`.
-	 * Each boolean value indicates whether to include a specific field in the query results.
-	 */
-	let passwordDetails: Prisma.SavePasswordSelect = {
-		password: true,
-		createdAt: true,
-		title: true,
-		details: true,
-		id: true
-	};
-	try {
-		// Retrieve and display saved passwords for the user
-		const displayPassword = await db.savePassword.findMany({
-			where: { userId: user.id },
-			select: passwordDetails,
-			orderBy: { createdAt: 'desc' }
-		});
-		return { displayPassword };
-	} catch (error) {
-		console.error(error);
-		return fail(400, { error: 'No passwords' });
-	}
-},
+	// View action to display saved passwords for a user
+	viewPasswords: async ({ request }) => {
+		const data = await request.formData();
+		let id = String(data.get('id'));
+		// Ensure id is a string or undefined, not null
+		// Check if the user exists in the database
+		const user = await db.user.findUnique({ where: { id } });
+		if (!user) {
+			return fail(404, { invalid: true });
+		}
+		/**
+		 * Specifies fields to select from password records using Prisma's Select added to the model name SavePassword.
+		 * Only works for select to give `SavePasswordSelect`.
+		 * Each boolean value indicates whether to include a specific field in the query results.
+		 */
+		let passwordDetails: Prisma.SavePasswordSelect = {
+			password: true,
+			createdAt: true,
+			title: true,
+			details: true,
+			id: true
+		};
+		try {
+			// Retrieve and display saved passwords for the user
+			const displayPassword = await db.savePassword.findMany({
+				where: { userId: user.id },
+				select: passwordDetails,
+				orderBy: { createdAt: 'desc' }
+			});
+			return { displayPassword };
+		} catch (error) {
+			console.error(error);
+			return fail(400, { error: 'No passwords' });
+		}
+	},
 	hidePasswords: async ({}) => {
 		return { displayPassword: [] };
 	}
