8e1b97a8444f769fc2a9cc44d1d1dd1b95ceb324
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-08-23 06:52:05 +0100

    feat ✨: add documentation for UpcomingFeaturesList component

--- src/routes/(auth)/Profile/UpcomingFeaturesList.svelte
+++ src/routes/(auth)/Profile/UpcomingFeaturesList.svelte
@@ -5,10 +5,22 @@
 
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 
-	import { projects, done ,truncateText} from '$lib';
-
-
+	import { projects, done, truncateText } from '$lib';
 </script>
+
+<!--
+@component
+## Svelte Component: Favorite App List
+This component is intended to display a list of a user's favorite mini-apps. Currently, it serves as a placeholder indicating that this feature is not yet implemented.
+
+## Key Features (Future)
+- **Display Favorite Apps**: Show a list of apps that the user has marked as favorites.
+- **Visual Indication**: Use a visual indicator, such as a star icon, to denote favorite apps.
+- **Interactive List**: Allow users to add or remove apps from their favorites list.
+
+## Current State
+As of now, the component displays a "None Yet" message, indicating that the favorite app functionality is not yet implemented.
+-->
 
 <Card>
 	<CardHeader>
