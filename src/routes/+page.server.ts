40dbb01c2fa3c198d74e63b2f8d6e82e2535682e
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-09-05 20:35:45 +0100

    Update +page.server.ts to reimplement redirect

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
-// import { redirect } from '@sveltejs/kit';
-// import type { PageServerLoad, Actions, RequestEvent } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions, RequestEvent } from './$types';
 
-// export const load: PageServerLoad = async (event) => {
-// 	if (event.url.href == 'https://svelte-mini-apps.netlify.app/') {
-// 		redirect(301, 'https://svelte-apps.me/');
-// 	}
-// };
+ export const load: PageServerLoad = async (event) => {
+ 	if (event.url.href == 'https://svelte-mini-apps.netlify.app/') {
+ 		redirect(301, 'https://svelte-apps.me/');
+ 	}
+};
 
 // export const actions: Actions = {
 // 	logout: () => {
