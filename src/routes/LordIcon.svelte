cd5cf7c40b15dd700971cece982ace0450652be7
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-09-10 14:17:16 +0100

    🔧 refactor ♻️(components): Update LordIcon class binding - Conditionally apply float direction.
    - Refactor class binding logic for the LordIcon component.
    - Apply float direction based on the 'dir' variable.
    - Enhance flexibility and maintainability of the component.

--- src/routes/LordIcon.svelte
+++ src/routes/LordIcon.svelte
@@ -10,5 +10,5 @@
 	src="https://cdn.lordicon.com/{src}.json"
 	{colors}
 	{trigger}
-	class="float-{dir} m-3 h-16 w-16 pt-5 md:h-24 md:w-24"
+	class="{dir === 'left' ? 'float-left' : 'float-right'} m-3 h-16 w-16 pt-5 md:h-24 md:w-24"
 ></lord-icon>
